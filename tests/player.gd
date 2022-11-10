66d21e38a2033278e79c9465b44804bc9293867c
Swarit Choudhari <68472469+SwiftyProgrammer690@users.noreply.github.com>
2022-11-10 14:23:20 -0500

    Update player.gd

--- tests/player.gd
+++ tests/player.gd
@@ -1,4 +1,12 @@
 extends KinematicBody2D
 
+var motion : Vector2D = Vector2D(0, 0)
+var bullet = preload("res://Scenes/Bullet.tscn")
+
 func _physics_process(delta):
-  pass
+  if Input.is_action_pressed("Move"):
+    motion.x += 1
+    
+func fire():
+  bullet_instance = bullet.instance()
+  new bullet_instance()
